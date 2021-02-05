0a7ca88b4b8fd19599cfe86088752cc56c891e3f
Charles Zipp <chzipp@microsoft.com>
2021-02-05 13:35:26 -0600

    removing terraform display task since cli can publish plans for display ui (#212)
    * removed terraform display task
    
    * remove display artifacts

--- tasks/terraform-display/src/commands/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as TerraformShowSummary } from './tf-show-summary'
-export { default as TerraformShow } from './tf-show'
--- tasks/terraform-display/src/core/tests/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as TaskRunner } from './task-runner';
-export { MockAnswers as MockAnswers } from './mock-answer-spy';
-export { requestedAnswers as requestedAnswers } from './mock-answer-spy';
-export { resetRequestedAnswers as resetRequestedAnswers } from './mock-answer-spy';
-export { TaskAnswers } from './task-answers';
