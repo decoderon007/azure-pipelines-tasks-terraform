6c656be14489be3c52c0baf7ddc5c3e4010348dd
Charles Zipp <chzipp@microsoft.com>
2021-02-11 12:10:08 -0600

    setting up test for component

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,9 @@ module.exports = {
     "transform": {
       "^.+\\.(ts|tsx)$": "ts-jest"
     },
+    "transformIgnorePatterns": [
+      "/!node_modules\\/azure-devops-ui/"
+    ],
     "coverageReporters": [ "text", "text-summary", "html", "cobertura"],
     "coverageDirectory": "./.tests/coverage",
     "collectCoverage": true
--- src/plan-summary-tab/plan-summary-tab.test.tsx
+++ src/plan-summary-tab/plan-summary-tab.test.tsx
@@ -1,5 +1,38 @@
-import { expect, test } from '@jest/globals'
+import { expect, test, describe, it } from '@jest/globals'
+import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import TerraformPlanDisplay, { NoPublishedPlanMessage } from './plan-summary-tab';
 
-test("basic", () => {
-  expect(true).toBe(true)
+test("render", () => {
+  expect(true).toBe(true);
+})
+
+let container: HTMLDivElement | null;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  if(container){    
+    unmountComponentAtNode(container);
+    container.remove();
+  }
+  container = null;
+})
+
+describe("no plans have been published", async () => {
+  //todo: mock out api to get attachments to return undefined...
+  await act(() => {
+    render(<TerraformPlanDisplay />, container);
+  });
+  it("renders with message indicating no plans were found", () => {
+    expect(true).toBe(true);
+    // const planElement = container?.querySelector("div.flex-grow");
+    // expect(planElement).not.toBeUndefined();
+    // expect(planElement?.innerHTML).toBe(NoPublishedPlanMessage);
+  });
+});
+
