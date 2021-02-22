47dc3594187136b1c72a07dfb6b38faaba5b6102
Charles Zipp <chzipp@microsoft.com>
2021-02-22 08:22:15 -0600

    show message when no plans exist (#226)
    * #201 setup unit test boilerplate
    
    * updated readme
    
    * updated readme
    
    * conditionally display plan
    
    * show message when no plan available
    
    * setting up test for component
    
    * --test not allowed, use webpack env
    
    * fix webpack module resolution error
    
    * update no plan found message
    
    * test can render component
    
    * refactoring ado sdk/api deps (not working yet)
    
    * abstracted attachments to enable unit testing
    
    * dont need mocks after abstracting attachments
    
    * put back test data

--- src/plan-summary-tab/plan-summary-tab.tsx
+++ src/plan-summary-tab/plan-summary-tab.tsx
@@ -1,77 +1,44 @@
-import {
-    default as AnsiUp
-} from 'ansi_up';
-import * as API from "azure-devops-extension-api";
-import { CommonServiceIds, getClient, IProjectPageService } from "azure-devops-extension-api";
-import { Build, BuildRestClient, BuildServiceIds, IBuildPageDataService, Timeline } from "azure-devops-extension-api/Build";
-
-import * as SDK from "azure-devops-extension-sdk";
-
+import { default as AnsiUp } from 'ansi_up';
 import { Card } from "azure-devops-ui/Card";
 import { ObservableArray, ObservableValue } from "azure-devops-ui/Core/Observable";
 import { Dropdown } from "azure-devops-ui/Dropdown";
 import { DropdownSelection } from "azure-devops-ui/Utilities/DropdownSelection";
 import { Observer } from "azure-devops-ui/Observer";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import "./plan-summary-tab.scss";
 import { IListBoxItem } from 'azure-devops-ui/ListBox';
-
-interface ThisBuild {
-    project: API.IProjectInfo,
-    buildId: number,
-    build: Build,
-    timeline: Timeline,
-}
+import { IAttachmentService } from '../services/attachments';
 
 interface TerraformPlan {
     name: string,
     plan: string,
 }
 
-class TerraformPlanDisplay extends React.Component {
+export const NoPublishedPlanMessage = "No terraform plans have been published for this pipeline run. The terraform cli task must run plan with <code>publishPlanResults: true</code> to view plans.";
 
-    private readonly buildClient: BuildRestClient
+export default class TerraformPlanDisplay extends React.Component<{ attachments: IAttachmentService }> {
+
+    private readonly attachments: IAttachmentService
     private readonly terraformPlanAttachmentType: string = "terraform-plan-results"
-    private readonly taskId: string = "721c3f90-d938-11e8-9d92-09d7594721b5"
 
     private planSelection = new DropdownSelection();
     private chosenPlan = new ObservableValue(-1);
     private plans = new ObservableArray<TerraformPlan>([]);
 
-    constructor(props: {} | Readonly<{}>) {
+    constructor(props: { attachments: IAttachmentService } | Readonly<{ attachments: IAttachmentService }>) {
         super(props)
-        this.buildClient = getClient(BuildRestClient)
+        this.attachments = props.attachments        
     }
 
     public async componentDidMount() {
 
         let foundPlans: TerraformPlan[] = [];
-
-        if (process.env.TEST) {
-            const testData = require('./test-data')
-            // Inject test values here
-            const plan = testData.examplePlan1 as string
-            foundPlans.push({
-                name: 'test_deploy.tfplan',
-                plan
-            });
-            foundPlans.push({
-                name: 'stage_deploy.tfplan',
-                plan
-            });
-        } else {
-            SDK.init()
-            const build = await this.getThisBuild()
-            const attachmentNames = await this.getPlanAttachmentNames(build);
-            for (const name of attachmentNames) {
-                const plan = await this.getPlainPlanAttachment(build, name);
-                foundPlans.push({
-                    name,
-                    plan
-                });
-            }
-        }
+        
+        const attachments = await this.attachments.getAttachments(this.terraformPlanAttachmentType);
+        attachments?.forEach(attachment => {
+            foundPlans.push({ name: attachment.name, plan: attachment.content});
+        });
+        
 
         this.plans.change(0, ...foundPlans)
         const initialSelection = foundPlans.length - 1
@@ -96,7 +63,7 @@ class TerraformPlanDisplay extends React.Component {
                                 }
                             });
 
-                            let html = "No Terraform plan detected.";
+                            let html = NoPublishedPlanMessage;
                             if (props.chosenPlan > -1) {
                                 const ansi_up = new AnsiUp()
                                 const planText = props.plans[props.chosenPlan].plan;
@@ -133,86 +100,4 @@ class TerraformPlanDisplay extends React.Component {
     private onSelect = (event: React.SyntheticEvent<HTMLElement>, item: IListBoxItem<{}>) => {
         this.chosenPlan.value = parseInt(item.id);
     };
-
-    private async getThisBuild(): Promise<ThisBuild> {
-        const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService)
-        const buildService = await SDK.getService<IBuildPageDataService>(BuildServiceIds.BuildPageDataService)
-        const projectFromContext = await projectService.getProject()
-        const buildFromContext = await buildService.getBuildPageData() //requires await to work eventhough does not return Promise
-
-        if (!projectFromContext || !buildFromContext) {
-            throw new Error('Not running in AzureDevops context.')
-        } else {
-            console.log(`Running for project ${projectFromContext.id} and build ${buildFromContext.build?.id.toString()}`)
-        }
-
-        if (!buildFromContext.build?.id) {
-            console.log("Cannot get build id.")
-            throw new Error('Cannot get build from page data')
-        }
-
-        const buildId = buildFromContext.build.id
-        const build = await this.getBuild(projectFromContext.name, buildId)
-        const timeline = await this.getBuildTimeline(projectFromContext.name, buildId)
-
-        return {
-            project: projectFromContext,
-            buildId: buildId,
-            build: build,
-            timeline: timeline
-        }
-    }
-
-    getRecordId(timeline: Timeline): string {
-        for (let record of timeline.records) {
-            if (record && record.task && record.task.id == this.taskId) {
-                return record.id
-            }
-        }
-        throw new Error(`Could not find record id.`)
-    }
-
-    async getBuild(project: string, buildId: number): Promise<Build> {
-        return await this.buildClient.getBuild(project, buildId)
-    }
-
-    async getBuildTimeline(project: string, buildId: number): Promise<Timeline> {
-        return await this.buildClient.getBuildTimeline(project, buildId)
-    }
-
-    async getPlanAttachmentNames(build: ThisBuild): Promise<string[]> {
-        const attachments = await this.buildClient.getAttachments(
-            build.project.id,
-            build.buildId,
-            this.terraformPlanAttachmentType
-        )
-        const attachmentNames = attachments.map(e => e.name)
-        return attachmentNames
-    }
-
-    async getAttachment(build: ThisBuild, attachmentType: string, attachmentName: string): Promise<string> {
-        const recordId = this.getRecordId(build.timeline)
-        const attachment = await this.buildClient.getAttachment(
-            build.project.id,
-            build.buildId,
-            build.timeline.id,
-            recordId,
-            attachmentType,
-            attachmentName)
-        const td = new TextDecoder()
-        return td.decode(attachment)
-    }
-
-    async getPlainPlanAttachment(build: ThisBuild, attachmentName: string): Promise<string> {
-        let attachment: string | undefined
-        try {
-            attachment = await this.getAttachment(build, this.terraformPlanAttachmentType, attachmentName)
-        } catch (e) {
-            throw new Error(`Failed to download plain plan: ${e}`)
-        }
-
-        return attachment;
-    }
 }
-
-ReactDOM.render(<TerraformPlanDisplay />, document.getElementById("root"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,70 @@
 const path = require("path");
-const fs = require("fs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const webpack = require('webpack');
-const yargs = require('yargs');
 
-const testCompile = yargs.boolean("test").argv;
-
-const entries = {};
-const srcDir = path.join(__dirname, "src");
-fs.readdirSync(srcDir)
-  .filter(dir => fs.statSync(path.join(srcDir, dir)).isDirectory())
-  .forEach(dir => (entries[dir] = "./" + path.join("src", dir, dir)));
-
-module.exports = {
-  target: "web",
-  entry: entries,
-  output: {
-    filename: "[name]/[name].js",
-    publicPath: "/dist/"
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    https: true,
-    port: 3000
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    alias: {
-      "azure-devops-extension-sdk": path.resolve(
-        "node_modules/azure-devops-extension-sdk"
-      )
-    }
-  },
-  stats: {
-    warnings: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader"
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "azure-devops-ui/buildScripts/css-variables-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.woff$/,
-        use: [
-          {
-            loader: "base64-inline-loader"
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: "file-loader"
+module.exports = (env) => {
+  return {
+    target: "web",
+    context: path.resolve(__dirname, './src'),
+    entry: './index.tsx',
+    output: {
+      filename: "index.js",
+      publicPath: "/dist/"
+    },
+    devtool: "inline-source-map",
+    devServer: {
+      https: true,
+      port: 3000
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      alias: {
+        "azure-devops-extension-sdk": path.resolve(
+          "node_modules/azure-devops-extension-sdk"
+        )
       }
+    },
+    stats: {
+      warnings: false
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader"
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader",
+            "css-loader",
+            "azure-devops-ui/buildScripts/css-variables-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.woff$/,
+          use: [
+            {
+              loader: "base64-inline-loader"
+            }
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: "file-loader"
+        }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([{ from: "**/*.html" }]),
+      new webpack.DefinePlugin({
+        'process.env.TEST': env.test
+      })
     ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([{ from: "**/*.html", context: "src" }]),
-    new webpack.DefinePlugin({
-      'process.env.TEST': testCompile.test
-    })
-  ]
+  }
 };
